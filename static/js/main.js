af49e9f92c521178355a12289308b87e5bf3f920
siwar <sewar.mahmou@gmail.com>
2023-10-13 15:26:15 +0200

    second commit

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+function CshangeTheme(theme) {
+    if (theme=='dark'){
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+    } else {
+        document.body.style.backgroundColor = '';
+        document.body.style.color = '';
+    }
+
+}
